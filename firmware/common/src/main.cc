f3a4866f52924aaa9d769309752c767e147a44f1
mkwiatko <mwkwiatko@gmail.com>
2017-04-25 17:51:37 -0700

    firmware/common/src/main.cc

---
+++ firmware/common/src/main.cc
@@ -0,0 +1,29 @@
+//////////////////////////////////////////////////////////////////////////////
+// This file is part of 'CPIX Development Firmware'.
+// It is subject to the license terms in the LICENSE.txt file found in the 
+// top-level directory of this distribution and at: 
+//    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+// No part of 'CPIX Development Firmware', including this file, 
+// may be copied, modified, propagated, or distributed except according to 
+// the terms contained in the LICENSE.txt file.
+//////////////////////////////////////////////////////////////////////////////
+#include <stdio.h>
+#include <string.h>
+#include "xil_types.h"
+#include "xil_io.h"
+#include "microblaze_sleep.h"
+#include "xil_printf.h"
+#include "regs.h"
+#include "ssi_printf.h"
+
+
+int main() { 
+   
+   while (1) {
+      
+   }
+   
+   
+   return 0;
+}
+

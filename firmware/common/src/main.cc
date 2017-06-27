ac5cb16e7c51b3e7682dae0b7dcc7db9f213580a
mkwiatko <mwkwiatko@gmail.com>
2017-06-27 11:35:57 -0700

    more debug printouts

--- firmware/common/src/main.cc
+++ firmware/common/src/main.cc
@@ -24,6 +24,17 @@ int main() {
    
    //xil_printf("Version %x\n\r",     Xil_In32(BUS_OFFSET+0x00000000));
    //xil_printf("PGP Bits %x\n\r",    Xil_In32(BUS_OFFSET+0x04000020));
+   //xil_printf("PGP RX CLK %d\n\r",    Xil_In32(BUS_OFFSET+0x04000064));
+   //xil_printf("PGP TX CLK %d\n\r",    Xil_In32(BUS_OFFSET+0x04000068));
+   //xil_printf("PGP RX cellErrorCount %d\n\r",    Xil_In32(BUS_OFFSET+0x04000028));
+   //xil_printf("PGP RX linkDownCount %d\n\r",    Xil_In32(BUS_OFFSET+0x0400002C));
+   //xil_printf("PGP RX linkErrorCount %d\n\r",    Xil_In32(BUS_OFFSET+0x04000030));
+   //xil_printf("PGP RX remOverflow0Cnt %d\n\r",    Xil_In32(BUS_OFFSET+0x04000034));
+   //xil_printf("PGP RX remOverflow1Cnt %d\n\r",    Xil_In32(BUS_OFFSET+0x04000038));
+   //xil_printf("PGP RX remOverflow2Cnt %d\n\r",    Xil_In32(BUS_OFFSET+0x0400003C));
+   //xil_printf("PGP RX remOverflow3Cnt %d\n\r",    Xil_In32(BUS_OFFSET+0x04000040));
+   //xil_printf("PGP RX frameErrCount %d\n\r",    Xil_In32(BUS_OFFSET+0x04000044));
+   //xil_printf("PGP RX frameCount %d\n\r",    Xil_In32(BUS_OFFSET+0x04000048));
    //xil_printf("MemTester rdy %x\n\r",    Xil_In32(BUS_OFFSET+0x03000100));
    //xil_printf("MemTester err %x\n\r",    Xil_In32(BUS_OFFSET+0x03000104));
    //xil_printf("MemTester wTimer %d\n\r",    Xil_In32(BUS_OFFSET+0x03000108));

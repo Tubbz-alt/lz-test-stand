79be9bd0f87511d7deb89ff226d6fc6cf4d244d2
mkwiatko <mwkwiatko@gmail.com>
2018-02-21 14:26:38 -0800

    updated sw

--- software/scripts/dataDevStatus.py
+++ software/scripts/dataDevStatus.py
@@ -38,110 +38,16 @@ import lztsViewer as vi
 from AdmPcieKu3Pgp2b import *
 
 
-
-
-    
-pgpVc0 = rogue.hardware.data.DataCard('/dev/datadev_0',0) # Registers for lzts board
-pgpVc1 = rogue.hardware.data.DataCard('/dev/datadev_0',1) # Data for lzts board
-
 memBase = rogue.hardware.data.DataMap('/dev/datadev_0')
 
 
-
-# Add data stream to file as channel 1
-# File writer
-dataWriter = pyrogue.utilities.fileio.StreamWriter(name='dataWriter')
-
-pyrogue.streamConnect(pgpVc1, dataWriter.getChannel(0x1))
-## Add pseudoscope to file writer
-#pyrogue.streamConnect(pgpVc2, dataWriter.getChannel(0x2))
-#pyrogue.streamConnect(pgpVc3, dataWriter.getChannel(0x3))
-
-cmd = rogue.protocols.srp.Cmd()
-pyrogue.streamConnect(cmd, pgpVc1)
-
-# Create and Connect SRP to VC1 to send commands
-srp = rogue.protocols.srp.SrpV3()
-pyrogue.streamConnectBiDir(pgpVc0,srp)
-
-
-#############################################
-# Microblaze console printout
-#############################################
-class MbDebug(rogue.interfaces.stream.Slave):
-
-    def __init__(self):
-        rogue.interfaces.stream.Slave.__init__(self)
-        self.enable = False
-
-    def _acceptFrame(self,frame):
-        if self.enable:
-            p = bytearray(frame.getPayload())
-            frame.read(p,0)
-            print('-------- Microblaze Console --------')
-            print(p.decode('utf-8'))
-
-#######################################
-# Custom run control
-#######################################
-
-class MyRunControl(pyrogue.RunControl):
-    def __init__(self,name):
-        pyrogue.RunControl.__init__(self,name=name,description='Run Controller LZTS', rates={1:'1 Hz', 10:'10 Hz', 30:'30 Hz'})
-        self._thread = None
-
-    def _setRunState(self,dev,var,value,changed):
-        if changed: 
-            if self.runState.get(read=False) == 'Running': 
-                self._thread = threading.Thread(target=self._run) 
-                self._thread.start() 
-            else: 
-                self._thread.join() 
-                self._thread = None 
-
-    def _run(self):
-        self.runCount.set(0) 
-        self._last = int(time.time()) 
- 
- 
-        while (self.runState.value() == 'Running'): 
-            delay = 1.0 / ({value: key for key,value in self.runRate.enum.items()}[self._runRate]) 
-            time.sleep(delay) 
-            self.root.Trigger() 
-  
-            self._runCount += 1 
-            if self._last != int(time.time()): 
-                self._last = int(time.time()) 
-                self.runCount._updated() 
-            
 ##############################
 # Set base
 ##############################
 class LztsBoard(pyrogue.Root):
-    def __init__(self, cmd, dataWriter, srp, **kwargs):
+    def __init__(self, **kwargs):
         
         pyrogue.Root.__init__(self, name='lztsBoard', description='LZTS Board')
-        
-        self.add(dataWriter)
-
-        # Add Devices
-        #self.add(fpga.Lzts(name='Lzts', offset=0, memBase=srp, hidden=False, enabled=True))
-
-        @self.command()
-        def Trigger():
-            cmd.sendCmd(1, 0)
-            cmd.sendCmd(0, 0)
-        
-        @self.command()
-        def GlobalRst():
-            cmd.sendCmd(2, 0)
-        
-        @self.command()
-        def GlobalSync():
-            cmd.sendCmd(3, 0)
-        
-        self.add(MyRunControl('runControl'))
-        #self.add(pyrogue.RunControl(name='runControl', rates={1:'1 Hz', 10:'10 Hz',30:'30 Hz'}, cmd=cmd.sendCmd(0, 0)))
         
         self.add(AdmPcieKu3Pgp2b(name='PCIE',memBase=memBase))        
         
@@ -149,13 +55,11 @@ class LztsBoard(pyrogue.Root):
         self.start(pyroGroup='lztsGui', pollEn=False)
         
 # Create board
-LztsBoard = LztsBoard(cmd, dataWriter, srp)
-
-
+LztsBoard = LztsBoard()
 
 # Create GUI
 appTop = PyQt4.QtGui.QApplication(sys.argv)
-guiTop = pyrogue.gui.GuiTop(group='lztsGui')
+guiTop = pyrogue.gui.GuiTop(group='pcieMon')
 guiTop.resize(800, 800)
 guiTop.addTree(LztsBoard)
 
@@ -165,6 +69,5 @@ appTop.exec_()
 
 # Close window and stop polling
 def stop():
-    mNode.stop()
     LztsBoard.stop()
     exit()

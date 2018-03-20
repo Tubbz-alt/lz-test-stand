682ba56f8aa7f6834e1fd0137ab89c68bb00158a
Larry Ruckman <ruckman@slac.stanford.edu>
2018-03-20 13:04:31 -0700

    updating script

--- software/scripts/lztsLoadFpga.py
+++ software/scripts/lztsLoadFpga.py
@@ -19,60 +19,34 @@
 # copied, modified, propagated, or distributed except according to the terms 
 # contained in the LICENSE.txt file.
 #-----------------------------------------------------------------------------
-import rogue.hardware.pgp
-import pyrogue.utilities.prbs
-import pyrogue.utilities.fileio
-import pyrogue.gui
-import surf
-import threading
-import signal
-import atexit
-import yaml
-import time
+
 import sys
-import argparse
+import pyrogue as pr
+import pyrogue.epics
+import pyrogue.gui
 import PyQt4.QtGui
-import PyQt4.QtCore
+import argparse
+import time
 import lztsFpga as fpga
-import lztsViewer as vi
-import operator
-import os
-from AdmPcieKu3Pgp2b import *
+
+#################################################################
 
 # Set the argument parser
 parser = argparse.ArgumentParser()
 
-
-def extant_file(x):
-    """
-    'Type' for argparse - checks that file exists but does not open.
-    """
-    if not os.path.exists(x):
-    #if not os.path.isfile("setup_template.csh"):
-        # Argparse uses the ArgumentTypeError to give a rejection message like:
-        # error: argument input: x does not exist
-        raise argparse.ArgumentTypeError("{0} does not exist".format(x))
-    else:
-        extension = os.path.splitext(x)[1]
-        if extension != '.mcs':
-            raise argparse.ArgumentTypeError("{0} not mcs file".format(x))
-    return x
-
 # Add arguments
 parser.add_argument(
-    "--f", 
-    type     = extant_file,
+    "--mcs", 
+    type     = str,
     required = True,
-    default  = True,
     help     = "path to mcs file",
 )
 
 parser.add_argument(
     "--l", 
     type     = int,
-    required = False,
-    default  = 0,
-    help     = "PGP lane number",
+    required = True,
+    help     = "PGP lane number [0 ~ 7]",
 )
 
 parser.add_argument(
@@ -82,154 +56,59 @@ parser.add_argument(
     help     = "define the PCIe card type (either pgp-gen3 or datadev-pgp2b)",
 )  
 
-
 # Get the arguments
 args = parser.parse_args()
 
-# Add PGP virtual channels
-if ( args.type == 'pgp-gen3' ):
-    # Create the PGP interfaces for ePix hr camera
-    pgpVc0 = rogue.hardware.pgp.PgpCard('/dev/pgpcard_0',args.l,0) # Registers for lzts board
-    pgpVc1 = rogue.hardware.pgp.PgpCard('/dev/pgpcard_0',args.l,1) # Data for lzts board
+#################################################################
 
-    print("")
-    print("PGP Card Version: %x" % (pgpVc0.getInfo().version))
-    
-elif ( args.type == 'datadev-pgp2b' ):
-    
-    #pgpVc0 = rogue.hardware.data.DataCard('/dev/datadev_0',(args.l*32)+0) # Registers for lzts board
+# Set base
+base = pr.Root(name='base',description='')    
+
+# Legacy PGP GEN3 Card
+if ( args.type == 'pgp-gen3' ):
+    pgpVc0 = rogue.hardware.pgp.PgpCard('/dev/pgpcard_0',args.l,0) # Registers for lzts board    
+# LZ's datadev PGP2B Card
+elif ( args.type == 'datadev-pgp2b' ):    
     pgpVc0 = rogue.hardware.data.DataCard('/dev/datadev_0',(7*32)+args.l) # Registers for lzts board
-    pgpVc1 = rogue.hardware.data.DataCard('/dev/datadev_0',(args.l*32)+1) # Data for lzts board
-    
-    #memBase = rogue.hardware.data.DataMap('/dev/datadev_0')
-    
-    
+# Undefined device type
 else:
     raise ValueError("Invalid type (%s)" % (args.type) )
-
-# Create the PGP interfaces for ePix camera
-#pgpVc0 = rogue.hardware.pgp.PgpCard('/dev/pgpcard_0',args.l,0) # Registers for lzts board
-#pgpVc1 = rogue.hardware.pgp.PgpCard('/dev/pgpcard_0',args.l,1) # Data for lzts board
-#pgpVc2 = rogue.hardware.pgp.PgpCard('/dev/pgpcard_0',0,2) # PseudoScope
-#pgpVc3 = rogue.hardware.pgp.PgpCard('/dev/pgpcard_0',0,3) # Monitoring (Slow ADC)
-
-
-# Add data stream to file as channel 1
-# File writer
-dataWriter = pyrogue.utilities.fileio.StreamWriter(name='dataWriter')
-
-pyrogue.streamConnect(pgpVc1, dataWriter.getChannel(0x1))
-## Add pseudoscope to file writer
-#pyrogue.streamConnect(pgpVc2, dataWriter.getChannel(0x2))
-#pyrogue.streamConnect(pgpVc3, dataWriter.getChannel(0x3))
-
-cmd = rogue.protocols.srp.Cmd()
-pyrogue.streamConnect(cmd, pgpVc1)
-
-# Create and Connect SRP to VC1 to send commands
+    
+# Create and Connect SRP to VC0 to send commands
 srp = rogue.protocols.srp.SrpV3()
-pyrogue.streamConnectBiDir(pgpVc0,srp)
+pyrogue.streamConnectBiDir(pgpVc0,srp)    
+    
+# Add Base Device
+base.add(fpga.Lzts(name='Lzts', memBase=srp))
 
+# Start the system
+base.start(pollEn=False)
+    
+# Create useful pointers
+AxiVersion = base.Lzts.AxiVersion
+PROM       = base.Lzts.MicronN25Q
 
-#############################################
-# Microblaze console printout
-#############################################
-class MbDebug(rogue.interfaces.stream.Slave):
+print ( '###################################################')
+print ( '#                 Old Firmware                    #')
+print ( '###################################################')
+AxiVersion.printStatus()
 
-    def __init__(self):
-        rogue.interfaces.stream.Slave.__init__(self)
-        self.enable = False
+# Program the FPGA's PROM
+PROM.LoadMcsFile(args.mcs)
 
-    def _acceptFrame(self,frame):
-        if self.enable:
-            p = bytearray(frame.getPayload())
-            frame.read(p,0)
-            print('-------- Microblaze Console --------')
-            print(p.decode('utf-8'))
+# Check if PROM successfully programmed
+if(PROM._progDone):
+    print('\nReloading FPGA firmware from PROM ....')
+    AxiVersion.FpgaReload()
+    time.sleep(10)
+    print('\nReloading FPGA done')
 
-#######################################
-# Custom run control
-#######################################
+    print ( '###################################################')
+    print ( '#                 New Firmware                    #')
+    print ( '###################################################')
+    AxiVersion.printStatus()
+else:
+    print('Failed to program FPGA')
 
-class MyRunControl(pyrogue.RunControl):
-    def __init__(self,name):
-        pyrogue.RunControl.__init__(self,name=name,description='Run Controller LZTS', rates={1:'1 Hz', 10:'10 Hz', 30:'30 Hz'})
-        self._thread = None
-
-    def _setRunState(self,dev,var,value,changed):
-        if changed: 
-            if self.runState.get(read=False) == 'Running': 
-                self._thread = threading.Thread(target=self._run) 
-                self._thread.start() 
-            else: 
-                self._thread.join() 
-                self._thread = None 
-
-    def _run(self):
-        self.runCount.set(0) 
-        self._last = int(time.time()) 
- 
- 
-        while (self.runState.value() == 'Running'): 
-            delay = 1.0 / ({value: key for key,value in self.runRate.enum.items()}[self._runRate]) 
-            time.sleep(delay) 
-            self.root.Trigger() 
-  
-            self._runCount += 1 
-            if self._last != int(time.time()): 
-                self._last = int(time.time()) 
-                self.runCount._updated() 
-            
-##############################
-# Set base
-##############################
-class LztsBoard(pyrogue.Root):
-    def __init__(self, cmd, dataWriter, srp, **kwargs):
-        
-        pyrogue.Root.__init__(self, name='lztsBoard', description='LZTS Board')
-        
-        self.add(dataWriter)
-
-        # Add Devices
-        self.add(fpga.Lzts(name='Lzts', offset=0, memBase=srp, hidden=False, enabled=True))
-
-        @self.command()
-        def Trigger():
-            cmd.sendCmd(0, 0)
-        
-        self.add(MyRunControl('runControl'))
-        #self.add(pyrogue.RunControl(name='runControl', rates={1:'1 Hz', 10:'10 Hz',30:'30 Hz'}, cmd=cmd.sendCmd(0, 0)))
-        
-        # Export remote objects
-        # disable polling (help with seg fault and bus errors?)
-        self.start(pyroGroup='lztsGui', pollEn=False)
-
-
-
-
-
-# Create board
-LztsBoard = LztsBoard(cmd, dataWriter, srp)
-
-# disable jestRx (help with seg fault and bus errors?)
-LztsBoard.Lzts.JesdRx.enable.set(False)
-
-
-print ('Old firmware:')
-LztsBoard.Lzts.AxiVersion.printStatus()
-
-# program flash
-LztsBoard.Lzts.MicronN25Q.LoadMcsFile(args.f)
-
-#reload FPGA
-print('Reloading FPGA')
-LztsBoard.Lzts.AxiVersion.FpgaReload()
-time.sleep(5)
-print('Reloading FPGA done')
-
-
-print ('New firmware:')
-LztsBoard.Lzts.AxiVersion.printStatus()
-
-LztsBoard.stop()
+base.stop()
 exit()
